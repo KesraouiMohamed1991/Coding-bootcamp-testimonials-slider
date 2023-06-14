9e775c3559a2a5cfebca62ecd7c4ebe858883c46
Kesraoui Mohamed <92538732+KesraouiMohamed1991@users.noreply.github.com>
2023-06-14 16:02:28 +0200

    Update app.js
    eddeting the images src

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function next() {
     if (ensemble.classList.contains('active')) {
         ensemble.classList.remove('active')
         seul.classList.add('active')
-        picture.style.backgroundImage = `url(/images/image-john.jpg)`
+        picture.style.backgroundImage = `url('https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80')`
 }
 }
 
@@ -17,7 +17,7 @@ let previous = function () {
     if (seul.classList.contains('active')) {
         seul.classList.remove('active')
         ensemble.classList.add('active')
-        picture.style.backgroundImage = `url(/images/image-tanya.jpg)`
+        picture.style.backgroundImage = `url('https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1064&q=80')`
     }
 }
 
